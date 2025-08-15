9248c4fff6000770c0bffe2ef5e867f8061ad353
AzzieFuzzie <mmatwadia01@gmail.com>
2025-08-15 17:46:41 +0200

    latest config

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,53 @@
 import path from 'path';
 import htmlmin from 'html-minifier';
+import EleventyVitePlugin from '@11ty/eleventy-plugin-vite';
+import glslifyPlugin from 'vite-plugin-glslify';
+import vitePluginClean from 'vite-plugin-clean';
 import pluginPug from '@11ty/eleventy-plugin-pug';
+// import imgix from './src/views/_data/imgix.js';
+
 
 export default function (eleventyConfig) {
+  //Global Data
+  // eleventyConfig.addGlobalData('imgix', imgix);
+
   // Set server port
   eleventyConfig.setServerOptions({ port: 3000 });
 
   // Add Pug plugin
   eleventyConfig.addPlugin(pluginPug);
+
+  // Vite plugin integration
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    tempFolderName: '.11ty-vite',
+    viteOptions: {
+      publicDir: 'public',
+      root: 'src',
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import "@styles/utils/variables.scss";`,
+          },
+        },
+      },
+      plugins: [
+        vitePluginClean({ targets: ['_site'] }),
+        glslifyPlugin(),
+      ],
+      resolve: {
+        alias: {
+          '@styles': path.resolve(process.cwd(), 'src/styles'),
+          '@app': path.resolve(process.cwd(), 'src/app'),
+          '@utils': path.resolve(process.cwd(), 'src/app/utils'),
+          '@components': path.resolve(process.cwd(), 'src/app/components'),
+          '@shaders': path.resolve(process.cwd(), 'src/app/shaders'),
+          '@classes': path.resolve(process.cwd(), 'src/app/classes'),
+          '@animations': path.resolve(process.cwd(), 'src/app/animations'),
+          '@pages': path.resolve(process.cwd(), 'src/app/pages'),
+        },
+      },
+    },
+  });
 
   // Passthrough copy for static assets
   eleventyConfig.addPassthroughCopy('public');
@@ -38,4 +78,4 @@ export default function (eleventyConfig) {
     passthroughFileCopy: true,
     htmlTemplateEngine: 'pug',
   };
-};
+}
