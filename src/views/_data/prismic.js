913eaa6fefc7141c9730936e0bf7c327f93956f5
AzzieFuzzie <mmatwadia01@gmail.com>
2025-08-19 23:45:28 +0200

    new pages

--- src/views/_data/prismic.js
+++ src/views/_data/prismic.js
@@ -13,19 +13,25 @@ const client = prismic.createClient(PRISMIC_REPO, {
 export default async function () {
   // const navigation = await client.getSingle("navigation");
   const home = await client.getSingle("home");
+  const menu = await client.getSingle("menu");
+  const about = await client.getSingle("about");
+  const contact = await client.getSingle("contact");
+  const book = await client.getSingle("book");
+  const footer = await client.getSingle("footer");
+
+  // Rich Text
+  const aboutHeadingsHTML = about.data.body
+    .filter(slice => slice.slice_type === 'intro_card')
+    .map(slice => asHTML(slice.primary.heading).replace(/\n/g, '<br>'));
+
 
 
   const homeHeadingsHTML = home.data.body
     .filter(slice => slice.slice_type === 'hero_card')
     .map(slice => asHTML(slice.primary.heading).replace(/\n/g, '<br>'));
 
-  const menu = await client.getSingle("menu");
-  const about = await client.getSingle("about");
-  const contact = await client.getSingle("contact");
-  const footer = await client.getSingle("footer");
-  console.log(homeHeadingsHTML);
 
-  return { home, menu, about, contact, footer, homeHeadingsHTML };
+  return { home, menu, about, contact, book, footer, homeHeadingsHTML, aboutHeadingsHTML };
 
 }
 
