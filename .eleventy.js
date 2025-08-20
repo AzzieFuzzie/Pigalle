6d370e33a405eb46896494844914c9b3305fc3f0
AzzieFuzzie <mmatwadia01@gmail.com>
2025-08-20 13:18:19 +0200

    config

--- .eleventy.js
+++ .eleventy.js
@@ -35,7 +35,7 @@ export default function (eleventyConfig) {
         },
       },
       plugins: [
-        glslifyPlugin(),
+
         // don't clean _site directly in Vercel builds
         // vitePluginClean({ targets: ['_site'] }),
       ],
