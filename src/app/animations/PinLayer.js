d3df9b0c464850c114767a6006c1c08dca3e9ce4
AzzieFuzzie <mmatwadia01@gmail.com>
2025-10-12 16:42:00 +0200

    improvemnts

--- src/app/animations/PinLayer.js
+++ src/app/animations/PinLayer.js
@@ -15,8 +15,8 @@ export default class PinLayer {
     // Create a GSAP tween that moves the element up while scrolling
     this.tween = GSAP.to(this.element, {
       // The animation you want to add
-      yPercent: -50, // Moves the element up by 50% of its own height. Adjust as you like.
-      ease: "linear",  // A linear ease works best with scrub
+      // yPercent: -50, // Moves the element up by 50% of its own height. Adjust as you like.
+      // ease: "linear",  // A linear ease works best with scrub
 
       // Your ScrollTrigger settings are now attached to the tween
       scrollTrigger: {
